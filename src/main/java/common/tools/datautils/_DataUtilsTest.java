c67fc9977946622907764d068ae57f7a902bf71e
lindechang <504828318@qq.com>
2016-03-04 11:03:31 +0800

    first commit

---
+++ src/main/java/common/tools/datautils/_DataUtilsTest.java
@@ -0,0 +1,15 @@
+package common.tools.datautils;
+
+import org.junit.Test;
+
+/**
+ * Created by lindec on 2016/3/4.
+ */
+public class _DataUtilsTest {
+
+
+    @Test
+    public void test1(){
+        System.out.println("hello world!");
+    }
+}
